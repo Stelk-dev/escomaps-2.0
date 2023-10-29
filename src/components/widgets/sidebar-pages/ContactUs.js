d2a30152b6c39d09517271aabc6c6a67e12e6602
Stefano <kstel30@gmail.com>
2023-10-29 18:20:35 +0100

    Adding UI pages

--- src/components/widgets/sidebar-pages/ContactUs.js
+++ src/components/widgets/sidebar-pages/ContactUs.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function ContactUs() {
   return (
-    <div className='center-class' style={{height: "100vh"}}>Contact us</div>
-  )
+    <div className="default-information-page">
+      <h1>Siamo qui per aiutarti</h1>
+      <br />
+      <div>
+        <div>
+          Per qualsiasi domande, esigenza, o problema generico puoi scriverci
+          all'indirizzo:
+        </div>
+        <a href="mailto:support@escomaps.com" target="_blank">
+          support@escomaps.com
+        </a>
+        <div>o al numero:</div>
+        <a href="https://wa.me/+39388098323" target="_blank">
+          +39 388 098 323
+        </a>
+      </div>
+
+      <div
+        style={{
+          fontStyle: "italic",
+          marginTop: "8px",
+          color: "grey",
+          fontSize: "14px",
+        }}
+      >
+        Ti risponderemo in meno di 24 ore lavorative
+      </div>
+    </div>
+  );
 }
