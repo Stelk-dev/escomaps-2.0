611898740eae558b2ed2d7936d72d11106ece806
Stefano <kstel30@gmail.com>
2024-01-24 17:48:53 +0100

    Adding search ads

--- src/components/home/SearchAds.js
+++ src/components/home/SearchAds.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdsList from "./widgets/AdsList";
 import { useLocation } from "react-router-dom";
 import SelectCityLocationModal from "./widgets/SelectCityLocationModal";
+import { GetAds, SearchAdsInDatabase } from "../../providers/AdsProvider";
+import { CircularProgress } from "@mui/material";
 
 export default function SearchAds() {
   const location = useLocation();
   const [showLocationModal, setShowLocationModal] = useState(false);
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get("q");
+  const [ads, setAds] = useState(null);
+
+  useEffect(() => {
+    console.log("SearchAdsInDatabase");
+    SearchAdsInDatabase(query).then((v) => {
+      if (v.length === 0)
+        GetAds.then((oldAds) => {
+          const data = oldAds.docs.map((a) => a.data());
+          setAds(data);
+        });
+      else setAds(v);
+    });
+  }, [query]);
 
   return (
     <div
@@ -28,9 +43,9 @@ export default function SearchAds() {
         }}
       >
         {/* Titles */}
-        <div>
+        <div style={{ marginBottom: "12px" }}>
           <h1 style={{ fontSize: "24px", fontWeight: "bold" }}>
-            {query.substring(0, 1).toUpperCase() + query.substring(1)} in Milano
+            {query.substring(0, 1).toUpperCase() + query.substring(1)}
           </h1>
           <div style={{ color: "grey", fontSize: "14px" }}>
             Vuoi cercare in una zona diversa?{" "}
@@ -49,12 +64,20 @@ export default function SearchAds() {
           </div>
         </div>
 
-        <div style={{ height: "8px" }} />
-
-        <br />
-
         {/* Ads list */}
-        <AdsList ads={[]} />
+        {ads === null ? (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <CircularProgress style={{ color: "white" }} />
+          </div>
+        ) : (
+          <AdsList ads={ads} />
+        )}
 
         <div style={{ height: "40px" }} />
       </div>
--- src/providers/AdsProvider.js
+++ src/providers/AdsProvider.js
@@ -4,4 +4,23 @@ const GetAds = GetDocs(advertisementsKey);
 
 const GetAdvData = (idAdv) => GetData(advertisementsKey, idAdv);
 
-export { GetAds, GetAdvData };
+async function SearchAdsInDatabase(query) {
+  const t = query.toLowerCase();
+  return GetAds.then((v) => {
+    return v.docs
+      .map((a) => a.data())
+      .filter((e) => {
+        return (
+          e.name.toLowerCase().includes(t) ||
+          e.categories.map((e) => e.toLowerCase()).includes(t) ||
+          e.services.map((e) => e.toLowerCase()).includes(t) ||
+          e.locationData.address.toLowerCase().includes(t) ||
+          e.phoneNumber.toLowerCase().includes(t) ||
+          e.title.toLowerCase().includes(t) ||
+          e.description.toLowerCase().includes(t)
+        );
+      });
+  });
+}
+
+export { GetAds, GetAdvData, SearchAdsInDatabase };
